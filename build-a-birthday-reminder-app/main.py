10abeb884bc2ab09e88007b0d30c385b59fbd956
amanda-bann <amanda.banner@gmail.com>
2025-11-13 19:31:26 -0700

    Make a simple birthday reminder app using elif and else statements

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,12 @@
+#This is a simple bithday reminder app. The user can input a name and the app will return the birthday of that person if it exists in the records.
+
 user_input = input("Which birthday are you looking for? ")
+
+if user_input == "Chloe":
+    print("Chloe's birthday is on January 2nd.")
+elif user_input == "Guy":
+    print("Guy's birthday is March 30th.")
+elif user_input == "Baba":
+    print("Baba's birthday is on January 25th.")
+else:
+    print("There is no record of that person's birthday.")
