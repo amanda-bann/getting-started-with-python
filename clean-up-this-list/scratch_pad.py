e91287077bc408c722d624bea5f7bc99886113b4
amanda-bann <amanda.banner@gmail.com>
2025-11-17 12:54:02 -0700

    Practice with lists

---
+++ clean-up-this-list/scratch_pad.py
@@ -0,0 +1,3 @@
+fav_country_music_artists = ["George Strait", "Willie Nelson", "Sturghill Simpson", "Chris Stapleton", "Bill Strings", "Sierra Ferrell", "Patsy Cline"]
+
+print(fav_country_music_artists[2:]) 
