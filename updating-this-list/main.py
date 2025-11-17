ddd483feffd33545cff7660dfec7dee82cb2131d
amanda-bann <amanda.banner@gmail.com>
2025-11-17 13:15:48 -0700

    Updating friends and family list

---
+++ updating-this-list/main.py
@@ -0,0 +1,15 @@
+friends_and_family = ["Guy", "James", "Roger", "Chloe", "Anna", "Kayla", "Willow", "Taylor", "Stacey", "Jeff", "Chris", "Meagan", "Marshall", "Olin", "Sarah", "Ben", "Shannon", "Sievers", "Cheyenne", "Sven"]
+
+print(len(friends_and_family))
+
+dinner_guests = friends_and_family[:4]
+print(dinner_guests)
+
+drinks_guest = friends_and_family[16:]
+print(drinks_guest)
+
+hiking_buddies = friends_and_family[10:14]
+print(hiking_buddies)
+
+dog_walkers = friends_and_family[14]
+print(dog_walkers)
