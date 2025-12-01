2a79f8c63981d0ddcf4b96d44dd91763c21b53fc
amanda-bann <amanda.banner@gmail.com>
2025-11-30 22:02:42 -0700

    Updates to the translator dictionary

--- build-a-language-translator/main.py
+++ build-a-language-translator/main.py
@@ -1,9 +1,50 @@
 translations = {
+  "bathroom":"baño",
+  "beer":"cerveza",
+  "bread":"pan",
+  "breakfast":"desayuno",
+  "cat":"gato",
+  "dad":"papá",
+  "dog":"perro",
+  "family":"familia",
+  "food":"comida",
+  "friend":"amigo",
+  "goodbye":"adiós",
   "hello":"hola",
+  "house":"casa",
+  "love":"amor",
+  "mom":"mamá",
+  "money":"dinero",
+  "moon":"luna",
+  "night":"noche",
+  "no":"no",
+  "please":"por favor",
+  "school":"escuela",
+  "sorry":"lo siento",
+  "sun":"sol",
   "thank you":"gracias",
-  "sorry":"lo siento"
+  "yes":"sí",
+  "water":"agua",
+  "welcome":"bienvenido",
+  "what":"qué",
+  "when":"cuándo",
+  "where":"dónde",
+  "who":"quién",
+  "why":"por qué",
+  "work":"trabajo"
 }
 
 done = False
 
 print('Type "done" at any time to exit')
+
+while not done:
+    word = input("Greetings! Type in an English word to translate: ")
+    word = word.lower()
+
+    if word == "done":
+        done = True
+    elif word in translations:
+        print(translations[word])
+    else:
+        print("Translation unavailable")
